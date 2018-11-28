af1c9ea17decdb121bf38820d7e8e9014e6af970
Zhongyang Zhu <yang5055@gmail.com>
2018-11-28 17:50:09 -0500

    Added comments

--- tweetgen.py
+++ tweetgen.py
@@ -3,3 +3,31 @@
     Automated Tweet Generator
 """
 
+#How should we do this boiz
+#For words, we should use feature structures, likelihood, and probability.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#Tweet generation algorithm
+    #We can classify certain celebrities with certain feature structures, such as "politics"
+    # or "feminism". Then, maybe we can prefer words w/ these feature structures in sentence gen.
