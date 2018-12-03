11aeb4b41e8d94f90308afe85358229d7f1276f1
Zhongyang Zhu <yang5055@gmail.com>
2018-12-03 00:17:20 -0500

    testing some python libraries

--- tweetgen.py
+++ tweetgen.py
@@ -6,6 +6,16 @@
 #How should we do this boiz
 #For words, we should use feature structures, likelihood, and probability.
 
+import sys
+import nltk
+import random
+import string
+import numpy as np
+#import re #if we need regex for whatever reason
+
+#Below imports for chatbot algorithm generation
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
 
 
 
@@ -17,17 +27,28 @@
 
 
 
+#TWEET GENERATION ALGORITHM
+"""RULES: 
 
+#SELF LEARNING: Save celebrity NN/NNP preferences and prefer those over other common nouns.
 
+"""
 
+sentence_tokenized = ["This", "is", "a", "sentence", "."]
+lemmer = nltk.stem.WordNetLemmatizer();
 
+def LemTokens(tokens):
+    return [lemmer.lemmatize(token) for token in tokens]
+remove_punct_dict = dict((ord(punct), None) for punct in string.punctuation)
 
+def LemNormalize(text):
+    return LemTokens(nltk.word_tokenize(text.lower().translate(remove_punct_dict)))
 
+f = open('test.txt', 'r', errors = 'ignore')
+raw = f.read()
+raw = raw.lower().replace('\n', ' ')
 
+sent_tokens = nltk.sent_tokenize(raw)
+word_tokens = nltk.word_tokenize(raw) 
 
-
-
-
-#Tweet generation algorithm
-    #We can classify certain celebrities with certain feature structures, such as "politics"
-    # or "feminism". Then, maybe we can prefer words w/ these feature structures in sentence gen.
+print(sent_tokens[:2], '\n')
