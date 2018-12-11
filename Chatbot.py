e5c8b58cc4fc2c7982726c8537e696acd0dcaaaa
Zhongyang Zhu <yang5055@gmail.com>
2018-12-11 00:24:51 -0500

    Deleted Tweet class and modified Chatbot class

--- Chatbot.py
+++ Chatbot.py
@@ -11,8 +11,11 @@ import re
 likelihood = import_dictionaries.likelihood_dict
 transitions = import_dictionaries.transitions_dict
 wordcount = import_dictionaries.wordcount_dict
+collection_words = nltk.corpus.wordnet #Collection of words used for synonym matching
 
 class Chatbot:
+    lemmatizer = nltk.stem.WordNetLemmatizer() #Lemmatizer that changes words to stem of word. Accepts string
+
     ###INITIATE CHATBOT
     def __init__(self, name):
         self.name = name
@@ -20,16 +23,7 @@ class Chatbot:
         self.tweets_history = list() #Will be a dictionary of dictionaries of past tweets (organized by topic)
     ###END INITIATE CHATBOT
 
-    #Tokenize tweet, identify POSes and features.
-    def tokenize_tweet(self, tweet):
-        tweet_tokenized = nltk.word_tokenize(tweet)
-        tweet_normalized = nltk.word_tokenize(tweet.lower())
 
-        #Identify POSes per token here. Get subject from hashtags/nouns
-
-
-        return tweet_tokenized
-    ###END TOKENIZE TWEET FUNCTION
 
     ###Generate a response based on topic
     #Tweet is a sentence
@@ -70,18 +64,11 @@ class Chatbot:
             char_limit += 1
         print(sentence)
 
-    #Answer to a question given a subject (extract it from tweet)
-    def generate_answer(self, topic):
+    def expand_stack(self):
         pass
 
-    #Generate a question
-    def generate_question(self, topic):
+    def choose_word(self):
         pass
 
-    #Generate a random tweet that this celebrity chatbot might realistically chat about
-    def generate_chat(self, tweet):
-        sentence = ""
-
-    #Generate a statement about a topic
-    def generate_statement(self, tweet):
-        pass
+    def compare_with_past_tweets(self, generated_tweet):
+        #Lemmatize generated tweet and compare cosine similarity with past tweets
