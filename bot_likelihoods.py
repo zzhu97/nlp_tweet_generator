45bc89f45037cf5f7b55ccb394e6ce1b78bec41e
Josh Seol <joshuaseol@gmail.com>
2018-12-21 03:04:07 -0500

    Added corpora and updated likelihood tables

--- bot_likelihoods.py
+++ bot_likelihoods.py
@@ -3,20 +3,69 @@
 
 import pickle
 import global_vars
+import sys
+from collections import defaultdict
 
 global list_of_likelihood_tables #list of dictionaries
 num_of_bots = global_vars.num_of_bots #number of bots --> number of dictionaries to import. If #bots>#dictionaries, use big_dictionary
 
 list_of_likelihood_tables = list()
 
+# nested defaultdict of likelihoodTable[POS][word] = number of occurences
+likelihoodTable = defaultdict(lambda: defaultdict(lambda: 0))
+
+# nested defaultdict of priorTable[prior word's POS][current word POS] = number of occurences
+priorTable = defaultdict(lambda: defaultdict(lambda: 0))
+
+# Constants for beginning and ending states
+BEGIN_SENT = "BEGIN_SENT"
+END_SENT = "END_SENT"
+
+
 #TODO: Create a bot's likelihood (Markov model) dictionary, dump it in a .pkl file, then retrieve it and insert it into a list.
     #Update big_dictionary from global_vars with new POSes and such
 
 ###
 #TODO: Create a bot's likelihood dictionary here and return it
-#Possible format: dict: word --> dict(nextword, possibility)
-def create_bot_likelihood(fileIn):
-    pass
+# Input File: fileIn = tagged version of celebrity's tweets (format of WORD_POS)
+# likelihoodTable[word][POS] = number of occurences as that POS
+
+def create_bot_dictionaries(fileIn):
+    trainingFile = open(fileIn, 'r') # training corpus
+
+    priorPOS = BEGIN_SENT
+
+    # Split words in a line into tokens in tempLine
+    for line in trainingFile:
+        if line != "\n":
+            tempLine = line.strip('\n')
+            tempLine = tempLine.split(' ')
+        else:
+            tempLine = line
+
+        for tempWord in tempLine:
+            # Remove quotation marks
+            word = tempWord.replace('"', '')
+            rightIndex = word.rfind('_')
+
+            # Parse out periods from tokens
+            if(word[rightIndex-1:rightIndex] == '.'):
+                print("Skipping: ", word[rightIndex-1:rightIndex])
+                posString = word[rightIndex+1:]
+                wordString = word[:rightIndex-1]
+                print("POS: ", posString, "Word: ", wordString)
+                likelihoodTable[posString][wordString] += 1
+
+                priorTable[priorPOS][posString] += 1
+
+            else:
+                posString = word[rightIndex+1:]
+                wordString = word[:rightIndex]
+                print("POS: ", posString, "Word: ", wordString)
+                likelihoodTable[posString][wordString] += 1
+
+                priorTable[priorPOS][posString] += 1
+
 ###
 
 ###
@@ -36,7 +85,7 @@ def main():
     export into .pkl file to update
 
     """
-    pass
+    create_bot_dictionaries(sys.argv[1])
 ###
 
 #Driver
