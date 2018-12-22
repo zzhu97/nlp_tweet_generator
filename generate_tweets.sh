672df10f33e654c33072a100b2ba443883198612
Josh Seol <joshuaseol@gmail.com>
2018-12-22 01:30:30 -0500

    Added Generate Tweets Script

---
+++ generate_tweets.sh
@@ -0,0 +1,23 @@
+echo "Barack Obama: "
+python3 tweetgen.py corpora/Tagged/barackobama_tweets_tagged.txt
+
+echo "Bill Gates: "
+python3 tweetgen.py corpora/Tagged/billgates_tweets_tagged.txt
+
+echo "CNN Breaking News:"
+python3 tweetgen.py corpora/Tagged/cnnbrk_tweets_tagged.txt
+
+echo "Elon Musk:"
+python3 tweetgen.py corpora/Tagged/elonmusk_tweets_tagged.txt
+
+echo "Kim Kardashian:"
+python3 tweetgen.py corpora/Tagged/kimkardashian_tweets_tagged.txt
+
+echo "Kevin Hart:"
+python3 tweetgen.py corpora/Tagged/kevinhart4real_tweets_tagged.txt
+
+echo "NASA:"
+python3 tweetgen.py corpora/Tagged/nasa_tweets_tagged.txt
+
+echo "Oprah:"
+python3 tweetgen.py corpora/Tagged/oprah_tweets_tagged.txt
