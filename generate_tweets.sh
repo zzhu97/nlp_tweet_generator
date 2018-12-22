8682d648e40a617d06e2b24c768f459fe319a2cd
Josh Seol <joshuaseol@gmail.com>
2018-12-22 02:54:51 -0500

    Added new personalities

--- generate_tweets.sh
+++ generate_tweets.sh
@@ -21,3 +21,18 @@ python3 tweetgen.py corpora/Tagged/nasa_tweets_tagged.txt
 
 echo "Oprah:"
 python3 tweetgen.py corpora/Tagged/oprah_tweets_tagged.txt
+
+echo "Wendy's:"
+python3 tweetgen.py corpora/Tagged/wendys_tweets_tagged.txt
+
+echo "Donald Trump:"
+python3 tweetgen.py corpora/Tagged/realdonaldtrump_tweets_tagged.txt
+
+echo "Kanye West:"
+python3 tweetgen.py corpora/Tagged/kanyewest_tweets_tagged.txt
+
+echo "DJ Khaled:"
+python3 tweetgen.py corpora/Tagged/djkhaled_tweets_tagged.txt
+
+echo "NBA:"
+python3 tweetgen.py corpora/Tagged/nba_tweets_tagged.txt
